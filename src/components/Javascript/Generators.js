82ecafc423855e319c6213358250b0b102561911
nialloc9 <nialloc9@gmail.com>
2018-01-22 22:30:56 +0000

    feat: added generator article

--- src/components/Javascript/Generators.js
+++ src/components/Javascript/Generators.js
@@ -104,8 +104,66 @@ multiplier.next(); // 1000
                 </CodeBlock>
 
                 <Block margin={`${remCalc(20)} 0`}>
-                    Generators can be used to create asyncronous code that allows the developer to stop and start when needed. This can especially useful when combined with promises.
-                    However, ES7 has a proposal for <SoftLink to="node/async-await">async/await</SoftLink> that is already supported in node which is easier to understand and much cleaner.
+                    Generators can be used to create asyncronous code that allows the developer to stop and start when needed. This can especially useful when combined with promises. Below we can see
+                    an example of using generators with promises.
+                </Block>
+
+                <CodeBlock margin={`${remCalc(20)} 0`}>
+                    {
+                        `
+
+import request from './common/request';
+
+/**
+ * makes an ajax request
+ * @param url
+ * @param params
+ */
+const ajaxCall = (url, params) => {
+    request({url, params, callback: (jsonResponse) => {
+        if(jsonResponse){
+            generator.next(jsonResponse);
+        }
+    }})
+};
+
+/**
+ * a generator that makes ajax requests.
+ */
+function *fetchGenerator (){
+
+    // get data from github
+    const response1 = yield ajaxCall("https://api.github.com/users/nialloc9", { method: "GET" });
+
+    // below code will only be called if the above ajax request returns data. This check is handled in ajaxCall
+    const { public_repos }  = response1;
+
+    const response2 = yield ajaxCall( "https://api.npmjs.org/downloads/point/last-year/redux-push", { method: "GET" });
+
+    const { downloads } = response2;
+
+    console.log("Niall has " + public_repos +  " repos on github and his package redux-push has " + downloads + " downloads in the last year.");
+}
+
+const generator = fetchGenerator();
+
+export default () => {
+    generator.next();
+};
+`
+                    }
+                </CodeBlock>
+
+                <Block margin={`${remCalc(20)} 0`}>
+                    Above we can see 3 functions. The first is a a helper function called request that is imported from a file in the common directory.
+                    This is just a helper function that will do an ajax request. The second, ajaxCall is a function that calls the request function. Look closely at what it does
+                    in it's callback function. It calls generator.next with a jsonResponse only if there is a response returned. If no response is returned it will not continue to the
+                    next yield in the generator. This can be used for when one ajax call is dependant on anothers response. If there was no response returned in the first it would cause
+                    an error in the second. In case your wondering hoisting in javascript allows us to use generator even though it is actually defined below. Now, let's look inside the fetch generator.
+                    First, we try to retrieve data from the github api. Crucially, we are using ajaxCall instead of a direct
+                    ajax request. Remember, this will check the response and decide whether to continue to the next yield. If no response is returned it will finish here and not continue. If there is a
+                    valid reponse we now call the npm api and then output a message using a combination of data from each. Generators are really powerful and allow us to do really cool things.
+                    Read about another awesome feature async/await <SoftLink to="node/async-await">here</SoftLink>.
                 </Block>
 
                 <Block margin={`${remCalc(20)} 0`}>
